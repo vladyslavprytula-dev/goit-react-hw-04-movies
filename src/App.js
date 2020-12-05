ab13edc9df8e33089a487b10ca7ec317d9d2ec76
VladyslavPrytula <vladyslavprytula.dev@gmail.com>
2020-12-06 01:12:40 +0200

    added lazy loading

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import AppBar from './Components/AppBar/AppBar';
 import Container from './Components/Container/Container';
 import routes from './routes';
-import HomePage from './views/HomePage';
-import MoviesPage from './views/MoviesPage';
-import MovieDetailsPage from './views/MovieDetailsPage';
-import Error from './Components/Error/Error';
 import './styles/base.scss';
+const HomePage = lazy(() =>
+  import('./views/HomePage' /*webpackChunkName: "home-page" */),
+);
+const MovieDetailsPage = lazy(() =>
+  import(
+    './views/MovieDetailsPage' /*webpackChunkName: "movie-details-page" */
+  ),
+);
+const MoviesPage = lazy(() =>
+  import('./views/MoviesPage' /*webpackChunkName: "movies-page" */),
+);
+const Error = lazy(() =>
+  import('./Components/Error/Error' /*webpackChunkName: "error" */),
+);
 const App = () => (
   <>
     <AppBar />
     <Container>
-      <Switch>
-        <Route path={routes.home} exact component={HomePage} />
-        <Route path={routes.MovieDetailsPage} component={MovieDetailsPage} />
-        <Route path={routes.movies} component={MoviesPage} />
-        <Route component={Error} />
-      </Switch>
+      <Suspense>
+        <Switch>
+          <Route path={routes.home} exact component={HomePage} />
+          <Route path={routes.MovieDetailsPage} component={MovieDetailsPage} />
+          <Route path={routes.movies} component={MoviesPage} />
+          <Route component={Error} />
+        </Switch>
+      </Suspense>
     </Container>
   </>
 );
